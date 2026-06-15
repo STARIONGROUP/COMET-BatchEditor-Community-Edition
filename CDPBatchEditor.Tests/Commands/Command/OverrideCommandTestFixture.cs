b5afb3c26476a578dc540d74c35ce9590d779d5d
Alexander van Delft <a.vandelft@rheagroup.com>
2026-06-15 14:07:35 +0200

    Remove headers

--- CDPBatchEditor.Tests/Commands/Command/OverrideCommandTestFixture.cs
+++ CDPBatchEditor.Tests/Commands/Command/OverrideCommandTestFixture.cs
@@ -1,23 +1,21 @@
 ﻿//  --------------------------------------------------------------------------------------------------------------------
 //  <copyright file="OverrideCommandTestFixture.cs" company="Starion Group S.A.">
 //     Copyright (c) 2015-2024 Starion Group S.A.
-// 
-//     Author: Nathanael Smiechowski, Alex Vorobiev, Alexander van Delft, Sam Gerené
-// 
+//
 //     This file is part of CDP4-COMET Batch Editor.
 //     The CDP4-COMET Batch Editor is a commandline application to perform batch operations on a
 //     ECSS-E-TM-10-25 Annex A and Annex C data source
-// 
+//
 //     The CDP4-COMET Batch Editor is free software; you can redistribute it and/or
 //     modify it under the terms of the GNU Lesser General Public
 //     License as published by the Free Software Foundation; either
 //     version 3 of the License, or any later version.
-// 
+//
 //     The CDP4-COMET Batch Editor is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //     GNU Lesser General License version 3 for more details.
-// 
+//
 //     You should have received a copy of the GNU Lesser General License
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //  </copyright>
@@ -60,14 +58,14 @@ namespace CDPBatchEditor.Tests.Commands.Command
 
             Assert.That(this.Transactions.Any(t => t.AddedThing
                     .Any(p =>
-                        p is ParameterOverride s 
+                        p is ParameterOverride s
                         && s.Owner == this.Domain
                         && s.ParameterType.ShortName == parameterUserFriendlyShortName)),
                 Is.True);
 
             Assert.That(this.Transactions.Any(t => t.UpdatedThing
-                    .Any(u => 
-                        u.Value is ElementUsage p 
+                    .Any(u =>
+                        u.Value is ElementUsage p
                         && p.ParameterOverride.Any(s => s.Owner.ShortName == this.Domain.ShortName)
                     )),
                 Is.True);
--- CDPBatchEditor.Tests/Commands/Command/StateCommandTestFixture.cs
+++ CDPBatchEditor.Tests/Commands/Command/StateCommandTestFixture.cs
@@ -1,23 +1,21 @@
 ﻿//  --------------------------------------------------------------------------------------------------------------------
 //  <copyright file="StateCommandTestFixture.cs" company="Starion Group S.A.">
 //     Copyright (c) 2015-2024 Starion Group S.A.
-// 
-//     Author: Nathanael Smiechowski, Alex Vorobiev, Alexander van Delft, Sam Gerené
-// 
+//
 //     This file is part of CDP4-COMET Batch Editor.
 //     The CDP4-COMET Batch Editor is a commandline application to perform batch operations on a
 //     ECSS-E-TM-10-25 Annex A and Annex C data source
-// 
+//
 //     The CDP4-COMET Batch Editor is free software; you can redistribute it and/or
 //     modify it under the terms of the GNU Lesser General Public
 //     License as published by the Free Software Foundation; either
 //     version 3 of the License, or any later version.
-// 
+//
 //     The CDP4-COMET Batch Editor is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //     GNU Lesser General License version 3 for more details.
-// 
+//
 //     You should have received a copy of the GNU Lesser General License
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //  </copyright>
